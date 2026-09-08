486b284d1606d04f5c090326a69be17eb7d5f67a
Habiciv <lucasgermanodafloresta@gmail.com>
2026-09-08 13:51:20 -0300

    Add files via upload

--- main.tsx
+++ main.tsx
@@ -2,4 +2,5 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Home from './app/page';
 import './app/globals.css';
+import './app/pro.css';
 createRoot(document.getElementById('root')!).render(<Home/>);
